34185a4002fa3ed45f24e526c5531c07e6801e5e
Lau Jun Xiang <2717836L@student.gla.ac.uk>
2022-10-21 00:05:03 +0800

    Learned some Layouts

---
+++ flutterlearn/lib/screens/location_detail/text_section.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class TextSection extends StatelessWidget {
+  final Color _color;
+
+  //constructor
+  TextSection(this._color);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(color: _color),
+      child: Text("hi"),
+    );
+  }
+}
